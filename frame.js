30b9fcf19c442588e7ba6b4a9c36f477a34e3108
Jonathan Garbee <jonathan@garbee.me>
2023-06-09 15:35:03 -0400

    Update frame.js
    Make button execute a fetch request against the agent URL

--- frame.js
+++ frame.js
@@ -1,3 +1,9 @@
 import { createAgent } from "http://localhost:8081/external.js";
 
 window.agent = createAgent('http://localhost:8081');
+
+const buttonTrigger = document.querySelector('button');
+
+buttonTrigger.addEventListener('click', () => {
+  agent.doFetch();
+});
